69ef21bc341f0a4adade22dc6edf567d8d760945
Bryan <bryan.soasti@mail.citytech.cuny.edu>
2018-03-18 22:19:07 -0400

    Add files via upload

---
+++ Quiz2_proposal/question_1/question_1.ino
@@ -0,0 +1,15 @@
+
+//QUESTION 1: Create a loop of different frequencies (any range)
+
+
+void setup() 
+{
+  
+  for (int i =200; 1<8000; i++) //loops from 200hz - 8000hz 
+  {
+  tone (9,360,4000); // 360hz for 4 seconds 
+  delay(10);
+  }
+
+}
+
---
+++ Quiz2_proposal/question_2/question_2.ino
@@ -0,0 +1,27 @@
+//QUESTION 2: Create a LED sequence with 5 leds that cycles randomly at different paces
+
+
+void setup() 
+
+  {
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+  }
+void loop()
+
+ {
+  digitalWrite(9,LOW);
+  delay(10);
+  digitalWrite(10,HIGH);
+  delay(233);
+  digitalWrite(11,LOW);
+  delay(100);
+  digitalWrite(12,HIGH);
+  delay(300);
+  digitalWrite(13,LOW);
+  delay(700);
+  }
+
